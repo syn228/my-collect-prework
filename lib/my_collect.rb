7a582cc520d7fdd6851c1bc9ac48d281c99d6e5d
Aaron Na <syn228@nyu.edu>
2018-04-13 23:05:50 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(languages)
   counter = 0
   emptyArray = []
   while counter < languages.length
-    emptyArray << yield (languages[counter])
+    emptyArray << yield(languages[counter])
     counter += 1
   end
   emptyArray
