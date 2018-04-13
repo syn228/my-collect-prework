472185614150a6710dff66ca701f4baed6c80241
Aaron Na <syn228@nyu.edu>
2018-04-13 23:00:35 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(languages)
   emptyArray2 = []
   counter = 0
   while counter < languages.length
-    yield  emptyArray1 << languages.upcase
+    yield emptyArray1 << languages.upcase
     yield emptyArray2 << languages.split(" ").first
     counter += 1
   end
