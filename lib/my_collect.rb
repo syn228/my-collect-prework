5b692f266a1775cb02a65dd52e2917e475284437
Aaron Na <syn228@nyu.edu>
2018-04-13 22:57:05 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,16 +1,9 @@
 def my_collect(languages)
+  emptyArray = []
   counter = 0
   while counter < languages.length
     yield  languages[counter].upcase
     counter += 1
   end
   languages
-end
-
-my_collect(languages) do |name|
-  name.split(" ").first
-end
-
-my_collect(languages) do |lang|
-  lang.upcase
 end
